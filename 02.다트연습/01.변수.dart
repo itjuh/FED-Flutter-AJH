29623f31e563286d7012b5df798127cb8d34534c
itjuh <jh.2144.9679@gmail.com>
2023-12-13 17:35:36 +0900

    flutter day2

--- 02.다트연습/01.변수.dart
+++ 02.다트연습/01.변수.dart
@@ -1,10 +1,22 @@
 void main(List<String> args) {
+
+  /**
+   * 다트언어는 변수형을 자동으로 인식하는 선언이 있음
+   * 1. var : 자동 형 선택 variable 
+   * 2. dynamic : 자율 형 선택
+   */
   var myHome = "내 집이야!";
   print(myHome);
   // void : 리턴값이 없음
   // var 자동 형 선택, 다른형을 재할당 할 수 없음
   myHome = '내 집이야?';
   print(myHome);
+  dynamic weId = 'hello';
+  print('나의 아이디는 $weId');
+  weId = 797979;
+  print('너의 아이디는 $weId');
+
+  // [ 문자형 출력 방법 : ${코드} $변수 ]
 
   /**
    * 다트언어는 변수의 형을 선언함이 기본이다.
@@ -27,7 +39,7 @@ void main(List<String> args) {
 
   // 2. 문자형
   String name = '톰 행크스';
-  print('나는 '+name+'입니다.');
+  print('나는 $name입니다.');
 
   // 3. 불린형
   bool isTrue = true;
@@ -40,9 +52,29 @@ void main(List<String> args) {
   print(locList[0]+'은/는 한국의 수도입니다');
   print(locList.length);
   // 4-2. Set {} 중복제거, 순서없음 
-  Set<int> evens = {2,3,4,5,6,7,8};
+  // .toList()해서 사용하기
+
+  // Set<int> evens = {2,4,6,8};
+  Set<dynamic> evens = {2,4,6,'짝수'};
+  List<dynamic> evens2 = evens.toList();
+  // var evens = {2,4,6,'짝수'};
   print(evens);
+  print(evens2[3]);
   // 4-3. Map {} 객체와 유사
   Map<String, String> actor = {'이름':'강동원','나이':'42'};
-  print('${actor.values}');
+  print('${actor.values.toList()[0]} 안녕');
+  print(actor.values.toList()[1]);
+
+  /**
+   * [ 다트의 형변환(Casting) ]
+   * 특정 데이터형에서 다른 형으로 변환하는 것을 흔히 캐스팅이라 함
+   * 1. 일반적인 형변환 : 변수형.parse(대상변수);
+   * 2. 메서드 체인방식 : 변수.to형()
+   * ex) array.toList()
+   * **주의> 숫자가 아닌 문자를 숫자로 변환 불가
+   */
+  String stNum = '777';
+  print('문자형 숫자: $stNum');
+  num result = 111 + int.parse(stNum);
+  print('111+777=$result');
 }
